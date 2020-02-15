440a72cacd84b0130f4a3398a4cedc88092127b0
ymgyt <yamaguchi7073xtt@gmail.com>
2020-02-15 17:52:30 +0900

    update cli flags

--- src/cli.rs
+++ src/cli.rs
@@ -9,51 +9,67 @@ impl ArgParser {
     pub fn parse(args: env::ArgsOs) -> ArgMatches<'static> {
         App::new("netspeed")
             .version(VERSION)
-            .about("measure tcp throughput")
-            .setting(AppSettings::ColorAuto)
-            .setting(AppSettings::ColoredHelp)
-            .setting(AppSettings::VersionlessSubcommands)
+            .about("Measure tcp throughput")
+            .global_setting(AppSettings::ColorAuto)
+            .global_setting(AppSettings::ColoredHelp)
+            .global_setting(AppSettings::VersionlessSubcommands)
             .arg(
                 Arg::with_name("verbose")
                     .long("verbose")
                     .short("v")
                     .multiple(true)
-                    .help("logging verbose"),
+                    .global(true)
+                    .help("Logging verbose"),
             )
             .arg(
                 Arg::with_name("address")
                     .long("addr")
                     .alias("address")
                     .short("a")
-                    .help("tcp listening/connection address")
+                    .help("Remote server address")
                     .takes_value(true)
-                    .default_value("localhost:5555")
-                    .global(true),
+                    .default_value("netspeed.ymgyt.io:5555"),
             )
             .arg(
                 Arg::with_name("duration")
                     .long("duration")
                     .alias("duration-seconds")
-                    .help("speed test duration seconds")
+                    .help("Speed test duration seconds(max: 10)")
                     .takes_value(true)
                     .default_value("3")
+                    .validator(|s| {
+                        let n = s.parse::<u8>().map_err(|err| format!("{}", err))?;
+                        if n > 10 {
+                            Err("Max duration exceeded (max: 10)".to_owned())
+                        } else {
+                            Ok(())
+                        }
+                    })
                     .value_name("SECONDS"),
             )
             .subcommand(
                 App::new("server")
-                    .setting(AppSettings::ColorAuto)
-                    .setting(AppSettings::ColoredHelp)
+                    .about("Server mode")
                     .arg(
                         Arg::with_name("run")
                             .index(1)
                             .required(true)
-                            .help("running server"),
+                            .help("Running server"),
+                    )
+                    .arg(
+                        Arg::with_name("address")
+                            .long("addr")
+                            .alias("address")
+                            .short("a")
+                            .help("Listening address")
+                            .takes_value(true)
+                            .default_value("localhost:5555"),
                     )
                     .arg(
                         Arg::with_name("max-threads")
                             .long("max-threads")
                             .alias("max-workers")
-                            .help("max concurrent threads/workers")
+                            .help("Max concurrent threads/workers")
                             .takes_value(true)
                             .default_value("100")
                             .value_name("NUMBER"),
