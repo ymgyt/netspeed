41487f59033e1e91a47814ff8ffdd5f6c2cb09b6
ymgyt <yamaguchi7073xtt@gmail.com>
2020-02-15 17:52:05 +0900

    change logging format

--- src/logger.rs
+++ src/logger.rs
@@ -2,10 +2,10 @@ use env_logger::fmt::Color;
 use log::{Level, LevelFilter};
 use std::io::Write;
 
-pub fn init(verbose: u64) {
+pub fn init(verbose: u64, is_server: bool) {
     let mut builder = env_logger::Builder::new();
 
-    builder.format(|buf, record| {
+    builder.format(move |buf, record| {
         let level_color = match record.level() {
             Level::Trace => Color::White,
             Level::Debug => Color::Blue,
@@ -16,22 +16,30 @@ pub fn init(verbose: u64) {
         let mut level_style = buf.style();
         level_style.set_color(level_color);
 
-        writeln!(
-            buf,
-            "{level:5} {time} {file:>10}:{line:<4} {args}",
-            level = level_style.value(record.level()),
-            time = chrono::Utc::now().to_rfc3339(),
-            file = &record.file().unwrap_or("____unknown")[4..],
-            line = record.line().unwrap_or(0),
-            args = record.args(),
-        )
+        if is_server {
+            writeln!(
+                buf,
+                "{level:5} {time} {file:>10}:{line:<4} {args}",
+                level = level_style.value(record.level()),
+                time = chrono::Utc::now().to_rfc3339(),
+                file = &record.file().unwrap_or("____unknown")[4..],
+                line = record.line().unwrap_or(0),
+                args = record.args(),
+            )
+        } else {
+            writeln!(
+                buf,
+                "{level:5} {args}",
+                level = level_style.value(record.level()),
+                args = record.args(),
+            )
+        }
     });
 
     builder.filter(
         None,
         match verbose {
-            // TODO: 開発注はDebugにしておく
-            0 => LevelFilter::Debug,
+            0 => LevelFilter::Info,
             1 => LevelFilter::Debug,
             _ => LevelFilter::Trace,
         },
